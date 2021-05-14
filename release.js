77a638c2d3d899892d845ccf3ab0efee583d05d0
leonardmgh <leonard@hansen-net.eu>
2021-05-14 02:38:54 +0200

    Removed imports

--- release.js
+++ release.js
@@ -1,10 +1,5 @@
 const { ethers } = require("ethers");
-const creds = require('./sheet_secret.json'); 
 const Samari = artifacts.require("Samari");
-const MultiSend = artifacts.require("MultiSend");
-
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-// Initialize the sheet - doc ID is the long id in the sheets URL
 
 module.exports = async function(callback) {
     try {
