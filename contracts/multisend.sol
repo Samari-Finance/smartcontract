7edf256c746d486ba47a0690db6e5e1fb5e60548
leonardmgh <leonard@hansen-net.eu>
2021-05-14 15:14:04 +0200

    Fix airdrop vunerability

--- contracts/multisend.sol
+++ contracts/multisend.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Address.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract MultiSend {
+contract MultiSend is Ownable{
     using Address for address;
 
-  function multiSend(address _token, address[] memory addresses, uint256[] memory amount) public  {
+  function multiSend(address _token, address[] memory addresses, uint256[] memory amount) public onlyOwner() {
     IERC20 token = IERC20(_token);
     require(addresses.length == amount.length, "Amount and address length must be the same!");
     require(addresses.length <= 50, "Maximum 50 transfers pr call are allowed!");
